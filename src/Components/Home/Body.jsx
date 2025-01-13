e49496941510a29a96117a91d06ce16ef2526760
Gianella-Velez <ari.24ghia20@gmail.com>
2025-01-12 20:48:03 -0400

    2025-01-12 componentes

---
+++ src/Components/Home/Body.jsx
@@ -0,0 +1,15 @@
+const Body = () => {
+    return (
+        <>
+            <div className="card">
+                <h1>Curso de React Modulo 7 </h1>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+
+        </>
+    );
+};
+
+export default Body;
