99ad66e4d7cfd77e7cf4e71762ccb143dcf1a1ff
Gianella-Velez <ari.24ghia20@gmail.com>
2025-01-15 23:13:16 -0400

    productos

--- src/Components/Product/ProductTable.jsx
+++ src/Components/Product/ProductTable.jsx
@@ -4,16 +4,10 @@ const ProductTable = () => {
     const fruits = [
         { nombre: "Apple", precio: 1 },
         { nombre: "Dragonfruit", precio: 1 },
-        { nombre: "PassionFruit", precio: 2 },
-        { nombre: "Apple", precio: 1 },
-        { nombre: "Dragonfruit", precio: 1 },
         { nombre: "PassionFruit", precio: 2 }
 
     ];
     const vegetables = [
-        { nombre: "Spinach", precio: 2 },
-        { nombre: "Pumpkin", precio: 4 },
-        { nombre: "Peas", precio: 1 },
         { nombre: "Spinach", precio: 2 },
         { nombre: "Pumpkin", precio: 4 },
         { nombre: "Peas", precio: 1 }
